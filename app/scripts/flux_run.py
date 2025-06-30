6f154a824613db81836e856cce294e7f9660ffbe
Lawrence <claudiuslawrence@gmail.com>
2025-06-30 19:49:32 +0400

    test

--- app/scripts/flux_run.py
+++ app/scripts/flux_run.py
@@ -1,6 +1,5 @@
-import torch
+import torch, uuid, os
 from diffusers import FluxPipeline
-import uuid
 
 pipe = FluxPipeline.from_pretrained(
     "black-forest-labs/FLUX.1-dev",
@@ -9,16 +8,17 @@ pipe = FluxPipeline.from_pretrained(
 )
 
 async def generate_image(prompt: str):
-    with torch.no_grad():
-        image = pipe(
-            prompt,
-            height=1024,
-            width=1024,
-            guidance_scale=3.5,
-            num_inference_steps=50,
-            max_sequence_length=512,
-            generator=torch.manual_seed(0)
-        ).images[0]
-    genI_name = f"./output/{str(uuid.uuid4())[:8]}.png"
+    image = pipe(
+        prompt,
+        height=1024,
+        width=1024,
+        guidance_scale=3.5,
+        num_inference_steps=50,
+        max_sequence_length=512,
+        generator=torch.manual_seed(0)
+    ).images[0]
+    output_dir = "/app/output"
+    os.makedirs(output_dir, exist_ok=True) 
+    genI_name = os.path.join(output_dir, f"{str(uuid.uuid4())[:8]}.png")
     image.save(genI_name)
     return genI_name
