41a61556713e19e72fa59b899055d75b52e1e101
Lawrence <claudiuslawrence@gmail.com>
2025-07-04 22:25:53 +0400

    add more debugs

--- app/scripts/flux_im2im.py
+++ app/scripts/flux_im2im.py
@@ -13,15 +13,18 @@ def generate_im2im_task(prompt: str, image_path: str, seed: int = None) -> dict:
         torch_dtype="torch.float16",
         device_map="balanced"
     )
-    print("Pipe loaded starting generating image")
     try:
         print(f"Went inside try")
         if seed is None:
             seed = random.randint(0, 999999)
-            print(f"seed#{seed}")
+            
         generator = torch.manual_seed(seed)
         input_image = load_image(image_path)
+
+        print(f"Image path: {input_image}")
+
         print(f"🔍 Starting inference with prompt: '{prompt}', seed: {seed}")
+
         image = pipe(
             prompt,
             height=1024,
@@ -30,8 +33,6 @@ def generate_im2im_task(prompt: str, image_path: str, seed: int = None) -> dict:
             guidance_scale=2.5,
             generator=generator
         ).images[0]
-        if not image or not image.images or image.images[0] is None:
-            raise ValueError("❌ Image generation returned empty result.")
         
         output_dir = "./output"
         os.makedirs(output_dir, exist_ok=True)
