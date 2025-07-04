58de64982bfed66ed0417681c600a80cef549df0
Lawrence <claudiuslawrence@gmail.com>
2025-07-04 23:37:48 +0400

    test

--- app/scripts/flux_im2im.py
+++ app/scripts/flux_im2im.py
@@ -7,7 +7,9 @@ from PIL import Image
 def generate_im2im_task(prompt: str, image_path: str, seed: int = None) -> dict:
     from huggingface_hub import login
     login(token=os.environ["HUGGINGFACE_TOKEN"])
-    
+
+
+
     print("Start loading pipeline!")
     pipe = FluxKontextPipeline.from_pretrained(
         "black-forest-labs/FLUX.1-Kontext-dev",
@@ -26,41 +28,41 @@ def generate_im2im_task(prompt: str, image_path: str, seed: int = None) -> dict:
     print(f"🧐 Input image loaded: {image_path}")
     print(f"🔍 Prompt: '{prompt}', Seed: {seed}")
 
+    # try:
+    result = pipe(
+        prompt,
+        height=1024,
+        width=1024,
+        image=input_image,
+        guidance_scale=2.5,
+        generator=generator
+    )
+
+    print(f"📦 Result keys: {result.keys()}")
+    image = result.images[0]
+    print(f"🖼️ Image type: {type(image)}")
+
+    if image is None:
+        print("⚠️ Image is None")
+        return {"status": "error", "error": "Generated image is None"}
+
+    output_dir = "/tmp/output"
+    os.makedirs(output_dir, exist_ok=True)
+    genI_name = os.path.join(output_dir, f"{str(uuid.uuid4())[:8]}.png")
+
     try:
-        result = pipe(
-            prompt,
-            height=1024,
-            width=1024,
-            image=input_image,
-            guidance_scale=2.5,
-            generator=generator
-        )
-
-        print(f"📦 Result keys: {result.keys()}")
-        image = result.images[0]
-        print(f"🖼️ Image type: {type(image)}")
-
-        if image is None:
-            print("⚠️ Image is None")
-            return {"status": "error", "error": "Generated image is None"}
-
-        output_dir = "/tmp/output"
-        os.makedirs(output_dir, exist_ok=True)
-        genI_name = os.path.join(output_dir, f"{str(uuid.uuid4())[:8]}.png")
-
-        try:
-            image.save(genI_name)
-            print(f"✅ Image saved to {genI_name} (Seed: {seed})")
-        except Exception as e:
-            print(f"❌ Failed to save image: {e}")
-            return {"status": "error", "error": f"Image save failed: {e}"}
-
-        return {
-            "status": "success",
-            "image_path": genI_name,
-            "seed": seed
-        }
-
+        image.save(genI_name)
+        print(f"✅ Image saved to {genI_name} (Seed: {seed})")
     except Exception as e:
-        return {"status": "error", "error": str(e)}
+        print(f"❌ Failed to save image: {e}")
+        return {"status": "error", "error": f"Image save failed: {e}"}
+
+    return {
+        "status": "success",
+        "image_path": genI_name,
+        "seed": seed
+    }
+
+    # except Exception as e:
+    #     return {"status": "error", "error": str(e)}
 
