4a93ecdf46972cb8f5e181cf8dbbf0dc7368cb84
Lawrence <claudiuslawrence@gmail.com>
2025-07-04 09:05:53 +0400

    rm conn

--- app/worker.py
+++ app/worker.py
@@ -1,12 +1,10 @@
 from redis import Redis
 from rq import Worker, Queue
-from rq.connections import Connection
 
 listen = ['flux_image_gen']
 redis_conn = Redis(host='redis', port=6379)
 
 if __name__ == '__main__':
-    with Connection(redis_conn):
-        queues = [Queue(name, connection=redis_conn) for name in listen]
-        worker = Worker(queues)
-        worker.work()
+    queues = [Queue(name, connection=redis_conn) for name in listen]
+    worker = Worker(queues, connection=redis_conn)
+    worker.work()
