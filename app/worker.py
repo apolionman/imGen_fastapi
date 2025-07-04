36dcda882c1f2be1952574c1beb30f42d541c766
Lawrence <claudiuslawrence@gmail.com>
2025-07-04 09:35:10 +0400

    test timeout

--- app/worker.py
+++ app/worker.py
@@ -6,5 +6,5 @@ redis_conn = Redis(host='redis', port=6379)
 
 if __name__ == '__main__':
     queues = [Queue(name, connection=redis_conn) for name in listen]
-    worker = Worker(queues, connection=redis_conn)
+    worker = Worker(queues, connection=redis_conn, default_job_timeout=1200)
     worker.work()
