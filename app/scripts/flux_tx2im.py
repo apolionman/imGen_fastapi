fae2a0a1da0c87a3c2563ee9ff9876eecdcbe743
Lawrence <claudiuslawrence@gmail.com>
2025-07-24 11:17:30 +0400

    changed to 16

--- app/scripts/flux_tx2im.py
+++ app/scripts/flux_tx2im.py
@@ -18,11 +18,10 @@ def generate_image_task(prompt: str,
 
     print("Start loading pipeline!")
     pipe = FluxPipeline.from_pretrained(
-    "black-forest-labs/FLUX.1-dev",
-    torch_dtype=torch.float16,
-    trust_remote_code=True,
+        "black-forest-labs/FLUX.1-dev",
+        torch_dtype=torch.float16,
+        device_map="balanced"
     )
-    pipe = pipe.to("cuda")
     print("Pipe loaded, starting image generation")
 
     # try:
